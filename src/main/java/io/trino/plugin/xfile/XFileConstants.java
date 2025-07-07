26d9e3117468ee53f86264a807f28c44cd18af44
simon.meng <simon.meng@foxmail.com>
2025-07-07 23:45:53 +0800

    update

--- src/main/java/io/trino/plugin/xfile/XFileConstants.java
+++ src/main/java/io/trino/plugin/xfile/XFileConstants.java
@@ -1,6 +1,6 @@
 package io.trino.plugin.xfile;
 
 public interface XFileConstants {
-    String FILE_TABLE_REGEX = "^(local|s3).*\\.(csv|gz|zip|parquet)$";
+    String FILE_TABLE_REGEX = "^(local|s3)://.*\\.(csv|gz|zip|parquet)$";
     String FILE_TABLE_CSV_REGEX = "^(local|s3).*\\.(csv|gz|zip)$";
 }
